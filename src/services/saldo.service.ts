88e88cfcc1c2d7d61e87e0cf06f4e80876c97b94
Mateus <frrmateu@gmail.com>
2024-07-01 21:14:33 -0300

    implementa saldo controller e service

---
+++ src/services/saldo.service.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+
+export default class SaldoService {
+  private prisma = new PrismaClient().$extends({
+    result: {
+      aluno: {
+        saldo: {
+          needs: { totalCompras: true, totalDepositos: true },
+          compute({ totalCompras, totalDepositos }) {
+            return totalDepositos - totalCompras;
+          },
+        },
+      },
+    },
+  });
+
+  async findAll() {
+    const alunos = await this.prisma.aluno.findMany({
+      select: {
+        id: true,
+        nome: true,
+        totalCompras: true,
+        numCompras: true,
+        saldo: true,
+      },
+    });
+
+    return alunos;
+  }
+}
