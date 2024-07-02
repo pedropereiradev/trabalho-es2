b6a3df79eb04743a8864960d6fa16bc31eedfb28
Pedro Pereira <pedro.2108@hotmail.com>
2024-07-01 21:06:59 -0300

    feat: implementa custom error

---
+++ src/utils/customError.ts
@@ -0,0 +1,10 @@
+export default class CustomError extends Error {
+  status: number;
+
+  constructor(message: string, status: number) {
+    super(message);
+
+    this.status = status;
+    this.name = 'CustomError';
+  }
+}
