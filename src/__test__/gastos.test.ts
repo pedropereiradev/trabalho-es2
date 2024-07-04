3614c34251ea38b84a804ecfea9f7f064d794e8a
eduardostxr <eduardostxr@gmail.com>
2024-07-03 23:02:38 -0300

    test: teste para gastos

--- src/__test__/gastos.test.ts
+++ src/__test__/gastos.test.ts
@@ -1,35 +1,48 @@
-import { expect, it } from 'vitest';
+import { describe, it, expect, beforeEach, vi } from 'vitest';
 import GastoService from '../services/gastos.service';
-import { describe } from 'node:test';
 
-const gastoService = new GastoService();
-
-describe('Gastos Service', () => {
-  const gasto = {
-    alunoId: 1,
-    valorLanche: 10,
-    lanche: 'Coxinha',
-    descricao: 'Lanche da tarde',
+vi.mock('../services/gastos.service', () => {
+  return {
+    default: vi.fn().mockImplementation(() => ({
+      create: vi.fn().mockImplementation((gasto) => Promise.resolve({ gasto: { ...gasto, id: 1 } })),
+      findAll: vi.fn().mockResolvedValue([
+        { id: 1, alunoId: 1, valorLanche: 10, lanche: 'Coxinha', descricao: 'Lanche da tarde' },
+        { id: 2, alunoId: 2, valorLanche: 15, lanche: 'Pastel', descricao: 'Lanche da manhã' },
+      ]),
+      delete: vi.fn().mockImplementation((id) => Promise.resolve({ message: 'Gasto deletado com sucesso', id })),
+    })),
   };
+});
 
-  it('should return created gasto', async () => {
-    const gastoCriado = await gastoService.create(gasto);
+describe('Service de Gastos com banco simulado', () => {
+  let gastoService: GastoService;
 
-    expect(gastoCriado.gasto.valorLanche).toBe(gasto.valorLanche);
-    expect(gastoCriado.gasto.lanche).toBe(gasto.lanche);
-    expect(gastoCriado.gasto.descricao).toBe(gasto.descricao);
+  beforeEach(() => {
+    gastoService = new GastoService() as GastoService;
   });
 
-  it('should return all gastos', async () => {
+  it('deve retornar o gasto inserido', async () => {
+    const gasto = {
+      alunoId: 1,
+      valorLanche: 10,
+      lanche: 'Coxinha',
+      descricao: 'Lanche da tarde',
+    };
+    const gastoCriado = await gastoService.create(gasto);
+
+    expect(gastoCriado.gasto).toEqual({ ...gasto, id: 1 });
+  });
+
+  it('deve retornar todos os gastos', async () => {
     const gastos = await gastoService.findAll();
 
-    expect(gastos.length).toBeGreaterThan(1);
+    expect(gastos).toHaveLength(2);
   });
 
-  it('should return deleted gasto', async () => {
-    const gastoCriado = await gastoService.create(gasto);
-    const gastoDeletado = await gastoService.delete(gastoCriado.gasto.id.toString());
+  it('deve retornar a mensagem de gasto deletado', async () => {
+    const idToDelete = '1';
+    const gastoDeletado = await gastoService.delete(idToDelete);
 
-    expect(gastoDeletado.gasto.id).toBe(gastoCriado.gasto.id);
+    expect(gastoDeletado).toEqual({ message: 'Gasto deletado com sucesso', id: idToDelete });
   });
 });
